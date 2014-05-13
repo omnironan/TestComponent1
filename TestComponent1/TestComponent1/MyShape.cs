9efb685b3b7a8cc070b82c1e54ba5145d8d4a0be
Omnironan <rshortall@omnifone.com>
2014-05-13 17:24:17 +0100

    Change created from solution outside this rep

---
+++ TestComponent1/TestComponent1/MyShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestComponent1
+{
+    public class MyShape
+    {
+        public string Name { get; private set; }
+
+        public MyShape(string name)
+        {
+            Name = name;
+        }
+    }
+}
